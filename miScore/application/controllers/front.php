ec6794affefb0eae7b4692a66afd1061e6ebf698
JOSE GONZALES <jz.gonzales@gmail.com>
2013-04-30 17:37:11 -0500

    Cambios en los metodos de llamado a las vistas.

--- miScore/application/controllers/front.php
+++ miScore/application/controllers/front.php
@@ -22,12 +22,11 @@ class Front extends CI_Controller {
 		$this->load->view('crearEquipo.html',$output);
 	}
 	
-	function _salidaMetEquipoTorneo($output = null)
+	function _llamadoVistaRelacionarTorneoEquipo($output = null)
 	{
 		$this->load->view('relacionarEquipos.html',$output);
 	}
 	
-
 	
 	function crearTorneo()
 	{
@@ -64,7 +63,7 @@ class Front extends CI_Controller {
 		$crud->set_field_upload('Torneo_idTorneo','Equipo_idEquipo');
 		$output = $crud->render();
 	
-		$this->_salidaMetEquipoTorneo($output);
+		$this->_llamadoVistaRelacionarTorneoEquipo($output);
 	}
 	
 }
